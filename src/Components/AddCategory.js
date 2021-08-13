b13f2a8180b45fa1911b482bc8929fa3302cb5d7
Ccf <innoplusdevs@gmail.com>
2021-08-13 10:56:40 -0400

    first commit

---
+++ src/Components/AddCategory.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import PropTypes from 'prop-types';
+
+export const AddCategory = ({ setCategories }) => {
+  const [inputValue, setInputValue] = useState();
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (inputValue.length > 2) {
+      setCategories((cats) => [inputValue, ...cats]);
+      setInputValue("");
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" value={inputValue} onChange={handleInputChange} placeholder="Introduzca el nombre del gif a buscar y presione enter"/>
+    </form>
+  );
+};
+
+AddCategory.propTypes = {
+  setCategories: PropTypes.func.isRequired
+}
